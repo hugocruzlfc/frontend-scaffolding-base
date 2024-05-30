62ea1d5ff4f410d41939c673c8ba231abe58f8e8
hugocruzlfc <hugocruzlfc@gmail.com>
2024-05-30 18:31:49 +0200

    chore: cleaning code

--- src/tests/components/signup-form.test.tsx
+++ src/tests/components/signup-form.test.tsx
@@ -28,16 +28,6 @@ describe('SingUpForm', () => {
   });
 
   it('calls onSubmit with correct data when form is submitted', async () => {
-    // render(<SingUpForm onSubmit={mockOnSubmit} />);
-
-    // fireEvent.input(screen.getByPlaceholderText(/username/i), {
-    //   target: { value: 'Jon Doe' },
-    // });
-
-    // fireEvent.click(screen.getByRole('button', { name: /register/i }));
-
-    // expect(mockOnSubmit).toHaveBeenCalledWith({ username: 'Jon Doe' });
-
     render(<SingUpForm onSubmit={mockOnSubmit} />);
 
     fireEvent.input(screen.getByPlaceholderText(/username/i), {
@@ -46,7 +36,7 @@ describe('SingUpForm', () => {
 
     fireEvent.click(screen.getByRole('button', { name: /register/i }));
 
-    await screen.findByRole('button', { name: /register/i }); // Asegurando que el botón esté en el DOM
+    await screen.findByRole('button', { name: /register/i });
 
     expect(mockOnSubmit).toHaveBeenCalledTimes(1);
     await waitFor(() => {
